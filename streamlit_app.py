8ab70b90013714d53241daef564e679645e0d725
Juliana Méndez Rojas <46967761+julianamendez11@users.noreply.github.com>
2024-09-26 10:43:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,11 +34,9 @@ except FileNotFoundError:
 
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("Cuesta AI Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a Cuesta chatbot that uses OpenAI's GPT-3.5 model to generate responses based on internal data. "
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
