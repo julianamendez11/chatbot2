823b33f834624ee92b44902e22064bad7926220d
Juliana Méndez Rojas <46967761+julianamendez11@users.noreply.github.com>
2024-09-26 11:18:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ else:
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input("What do you want to know about Cuesta Skills?"):
 
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
